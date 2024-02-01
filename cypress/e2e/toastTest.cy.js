d25e2b78b9c7ee0d630a0487fbb23c177ca20724
pe4enka22 <missis.bug@gmail.com>
2024-02-01 23:48:15 +0200

    not working last 3 tests due to position

--- cypress/e2e/toastTest.cy.js
+++ cypress/e2e/toastTest.cy.js
@@ -47,8 +47,6 @@ let pageData = [{
   toastPosition: "ng-tns-c209-57",
   toastIcon: '<path d=\"M11.11 23a1 1 0 0 1-.34-.06 1 1 0 0 1-.65-1.05l.77-7.09H5a1 1 0 0 1-.83-1.56l7.89-11.8a1 1 0 0 1 1.17-.38 1 1 0 0 1 .65 1l-.77 7.14H19a1 1 0 0 1 .83 1.56l-7.89 11.8a1 1 0 0 1-.83.44zM6.87 12.8H12a1 1 0 0 1 .74.33 1 1 0 0 1 .25.78l-.45 4.15 4.59-6.86H12a1 1 0 0 1-1-1.11l.45-4.15z"></path>'
 }]
-
-
  describe("beforeEach method", () => {
 beforeEach(() => {
   cy.log('Open main page');
@@ -61,8 +59,6 @@ cy.log('Open Modal & Overlays page');
  cy.get('[href="/pages/modal-overlays/toastr"]').click();
 
    })
-
-
 
    pageData.forEach(pageData => {
      it('Expect', () => {
@@ -94,7 +90,7 @@ cy.log('Open Modal & Overlays page');
 
        cy.get('nb-card-footer button').first().click();
        cy.get('nb-toast').should('have.css', 'background-color', `${pageData.toastColor}`);
-      // cy.get('nb-toast').should('have.class', `${pageData.toastPosition}`);
+       cy.get('nb-toast').should('have.class', `${pageData.toastPosition}`);
        cy.get('nb-toast path').should('have.prop', 'outerHTML', `${pageData.toastIcon}`);
        cy.get('nb-toast> .content-container').should("contain.text", `${pageData.title}${pageData.content}`)
 
@@ -105,9 +101,6 @@ cy.log('Open Modal & Overlays page');
 
 })
 
-
-
-//})
 
 
 
